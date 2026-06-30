4eb6712dad2a9bb9f0fb8d5bd3a7657c02449f19
samriddhichandra <samriddhic62@gmail.com>
2026-06-30 14:13:40 +0530

    Add Largest Element Code

---
+++ Largest Element in an Array.java
@@ -0,0 +1,26 @@
+/*  1. Find the Largest Element in an Array
+
+Problem:
+Given an array, find the maximum element.
+
+Example:
+
+Input: [4, 7, 1, 9, 3]
+Output: 9
+
+Concepts tested:
+
+Traversing array
+Comparison logic
+Time complexity O(n) */
+
+import java.util.*;
+Class java{
+    void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an element:");
+        for(int i = 0; i<= Arr.length(); i++){
+        int Arr [] = 
+    }
+}
+}
